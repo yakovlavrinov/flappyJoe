0b99c95e6e7f27709bd29f561cfc1367b222699e
Yakov Lavrinov <lawrinov.yackov@yandex.ru>
2026-01-28 23:02:10 +0300

    refactoring: remove manifest and sw

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,3 @@ import Phaser from 'phaser'
 import { gameConfig } from './config'
 
 new Phaser.Game(gameConfig)
-
-// if ('serviceWorker' in navigator && !window.ysdk) {
-//   window.addEventListener('load', () => {
-//     navigator.serviceWorker
-//       .register('/sw.js')
-//       .then(() => console.log('Service Worker registered'))
-//       .catch((err) => console.error('SW registration failed:', err))
-//   })
-// }
